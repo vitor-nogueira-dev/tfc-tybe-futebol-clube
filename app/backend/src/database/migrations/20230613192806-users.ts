6b2df16341cffb2fd6d4b070cb64a8c5fcffb492
Vitor Nogueira <vslnogueira96@gmail.com>
2023-06-13 16:37:29 -0300

    feat: requirement 06
    Desenvolva em /app/backend/src/database nas pastas correspondentes, uma migration e um model para a tabela de pessoas usuárias

---
+++ app/backend/src/database/migrations/20230613192806-users.ts
@@ -0,0 +1,36 @@
+'use strict';
+
+import { Model, QueryInterface, INTEGER, STRING } from "sequelize";
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model>('users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        type: INTEGER,
+      },
+      username: {
+        allowNull: false,
+        type: STRING,
+      },
+      role: {
+        allowNull: false,
+        type: STRING,
+      },
+      email: {
+        allowNull: false,
+        type: STRING,
+      },
+      password: {
+        allowNull: false,
+        type: STRING,
+      }
+    })
+  },
+
+  down(queryInterface: QueryInterface) {
+    return queryInterface.dropTable('users');
+  },
+};
