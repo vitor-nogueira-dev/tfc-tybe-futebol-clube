6fafc76f57864c5e61e9632cb7615aec251bf0e3
Vitor Nogueira <vslnogueira96@gmail.com>
2023-06-13 16:01:50 -0300

    feat: requirement 02
    (TDD) Desenvolva testes que cubram no mínimo 5 por cento dos arquivos em /app/backend/src, com um mínimo de 7 linhas cobertas

---
+++ app/backend/src/tests/teams.test.ts
@@ -0,0 +1,23 @@
+// import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+
+import { teamsMock } from './mocks/teamsMock';
+import { teamIdMock } from './mocks/teamsMock'
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Usando o método GET em /teams & /teams/:id', function () {
+  it('Retorna a quantidade total de teams', async function () {
+    const response = await chai.request(app).get('/teams');
+
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.deep.equal(teamsMock);
+  });
+});
+
