db9a7b9a0ccc2bd284719c5722c0f5ca2a51cb29
Vitor Nogueira <vslnogueira96@gmail.com>
2023-06-13 21:26:00 -0300

    feat: requirement 19
    (TDD) Desenvolva testes que cubram no mínimo 60 por cento dos arquivos em /app/backend/src, com um mínimo de 80 linhas cobertas

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -4,16 +4,18 @@ import * as sinon from 'sinon';
 
 // @ts-ignore
 import chaiHttp = require('chai-http');
-import matches, { allMatches, matchesInProgress, updateGoals } from './mocks/matches.mocks';
+import matches, { allMatches, matchFail, matchesInProgress, updateGoals } from './mocks/matches.mocks';
 
 import { app } from '../app';
 import MatchesModel from '../database/models/MatchesModel';
 import { jwtPayload } from './mocks/login.mocks';
+import TeamsModel from '../database/models/TeamsModel';
 
 chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Matches', function () {
+  afterEach(() => sinon.restore());
   describe('testing router GET /matches', () => {
     it('should return status 200 and list all matches', async function () {
       const response = await chai.request(app).get('/matches');
@@ -53,15 +55,38 @@ describe('Matches', function () {
       expect(response.status).to.be.equal(200);
       expect(response.body).to.be.deep.equal({ message: 'Finished' });
     });
-    it('should return status 404 and a message', async () => {
-      sinon.stub(jwt, 'verify').callsFake(() => jwtPayload);
-      sinon.stub(MatchesModel, 'findOne').resolves(undefined);
+    // it('should return status 404 and a message', async () => {
+    //   sinon.stub(jwt, 'verify').callsFake(() => jwtPayload);
+    //   sinon.stub(MatchesModel, 'findOne').resolves(undefined);
 
-      const response = await chai.request(app).patch('/matches/60/finish')
+    //   const response = await chai.request(app).patch('/matches/60/finish')
+    //     .set('Authorization', 'token-valid');
+
+    //   expect(response.status).to.be.equal(404);
+    //   expect(response.body).to.be.deep.equal({ message: 'Team does not exist' });
+    // });
+  });
+  describe('testing router PATCH /matches/:id', () => {
+    it('should return status 400 and a message', async () => {
+      sinon.stub(jwt, 'verify').callsFake(() => jwtPayload);
+
+      const response = await chai.request(app).patch('/matches/1')
         .set('Authorization', 'token-valid');
 
-      expect(response.status).to.be.equal(404);
-      expect(response.body).to.be.deep.equal({ message: 'Team does not exist' });
+      expect(response.status).to.be.equal(400);
+      expect(response.body).to.be.deep.equal({ message: 'All fields must be filled' });
+    });
+    it('should return status 200 and a message', async () => {
+      sinon.stub(jwt, 'verify').callsFake(() => jwtPayload);
+      sinon.stub(MatchesModel, 'findOne').resolves(allMatches[1] as MatchesModel);
+      sinon.stub(MatchesModel, 'update').resolves([1]);
+
+      const response = await chai.request(app).patch('/matches/1')
+        .set('Authorization', 'token-valid')
+        .send(updateGoals);
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.be.deep.equal({ message: 'Updated' });
     });
   });
   describe('testing router PATCH /matches/:id', () => {
@@ -85,6 +110,26 @@ describe('Matches', function () {
 
       expect(response.status).to.be.equal(200);
       expect(response.body).to.be.deep.equal({ message: 'Updated' });
+    });
+  });
+  describe('testing router POST /matches', () => {
+    it('should return status 400 and a message', async () => {
+      sinon.stub(jwt, 'verify').callsFake(() => jwtPayload);
+
+      const response = await chai.request(app).post('/matches').set('Authorization', 'token-valid');
+
+      expect(response.status).to.be.equal(400);
+      expect(response.body).to.be.deep.equal({ message: 'All fields must be filled' });
+    });
+    it('should return status 422 and a message', async () => {
+      sinon.stub(jwt, 'verify').callsFake(() => jwtPayload);
+
+        const response = await chai.request(app).post('/matches')
+          .set('Authorization', 'token-valid')
+          .send(matchFail);
+
+        expect(response.status).to.be.equal(422);
+        expect(response.body).to.be.deep.equal({ message: 'It is not possible to create a match with two equal teams' });
     });
   });
 })
