bf6b93e48796e45344072bd7b6284812affedeb8
Vitor Nogueira <vslnogueira96@gmail.com>
2023-07-03 10:13:25 -0300

    feat: adding stubs tests

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -2,10 +2,12 @@
 import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
+import * as sinon from 'sinon';
 
 import { app } from '../app';
 
 import { teamsMock, teamIdMock } from './mocks/teamsMock';
+import TeamsModel from '../database/models/TeamsModel';
 
 chai.use(chaiHttp);
 
@@ -13,12 +15,14 @@ const { expect } = chai;
 
 describe('Usando o método GET em /teams & /teams/:id', function () {
   it('Retorna a quantidade total de teams', async function () {
+    sinon.stub(TeamsModel, 'findAll').resolves(teamsMock as TeamsModel[])
     const response = await chai.request(app).get('/teams');
 
     expect(response.status).to.be.equal(200);
     expect(response.body).to.deep.equal(teamsMock);
   });
-  it('Retorna a team baseado no ID', async function () {
+  it.only('Retorna a team baseado no ID', async function () {
+    sinon.stub(TeamsModel, 'findByPk').resolves(teamsMock[0] as TeamsModel)
     const response = await chai.request(app).get('/teams/1');
 
     expect(response.status).to.be.equal(200);
